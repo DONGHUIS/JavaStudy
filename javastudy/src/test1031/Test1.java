2201e9376c71503851fe2aa67ee697b75aa842e1
DONGHUIS <134819640+DONGHUIS@users.noreply.github.com>
2023-10-31 17:03:09 +0900

    푼문제.

--- javastudy/src/test1031/Test1.java
+++ javastudy/src/test1031/Test1.java
@@ -1,5 +1,50 @@
 package test1031;
+/*1. Card 클래스가 다음과 같을때 구동 클래스를 완성하시오
+
+[결과]
+Heart:1(100,250)
+Spade:1(100,250)
+Heart:1(50,80)
+Spade:1(50,80)
+*/
+class Card {
+	String kind;
+	int number;
+	static int width = 100;
+	static int height = 250;
+	public String toString() {
+		return kind + ":" + number + "(" + width + "," + height + ")";
+	}
+}
 
 public class Test1 {
-
+	public static void main(String[] args) {
+		Card c = new Card();
+		c.kind ="Heart";
+		c.number=1;
+		c.width=100;
+		c.height=250;
+		System.out.println(c);
+		
+		Card c1 = new Card();
+		c1.kind ="Spade";
+		c1.number=1;
+		c1.width=100;
+		c1.height=250;
+		System.out.println(c1);
+		
+		Card c2 = new Card();
+		c2.kind ="Heart";
+		c2.number=1;
+		c2.width=50;
+		c2.height=80;
+		System.out.println(c2);
+		
+		Card c3 = new Card();
+		c3.kind ="Spade";
+		c3.number=1;
+		c3.width=50;
+		c3.height=80;
+		System.out.println(c3);
+	}
 }
--- javastudy/src/test1031/Test4.java
+++ javastudy/src/test1031/Test4.java
@@ -1,5 +1,29 @@
 package test1031;
-
+/*
+4. 다음과 같은 멤버 변수를 갖는 Student 클래스를 구현 하시오
+   멤버 변수
+          String name //학생이름
+          int ban,no,kor,eng,math  //반,번호,국어점수,영어점수,수학점수
+   멤버 메서드
+          1. getTotal
+             기능 : 국어,영어,수학 점수의 합 반환
+              리턴타입 : int
+           2. getAverage
+              기능 : 총점(국어,영어,수학 점수의 합) 을 과목수로 나눈 평균 반환
+              리턴타입 : float
+ */
 public class Test4 {
-
+	public static void main(String[] args) {
+		Student s1 =new Student();
+		s1.name = "홍길동";
+		s1.ban = 1;
+		s1.no = 1;
+		s1.kor = 100;
+		s1.eng = 80;
+		s1.math = 85;
+		
+		System.out.printf("번호:%s,이름:%s,반:%s,국어:%d,수학:%d,영어:%d \n",s1.no,s1.name,s1.ban,s1.kor,s1.math,s1.eng);
+		System.out.print("총합:"+s1.getTotal()+",");
+		System.out.println("평균:"+s1.getAverage());
+	}
 }
