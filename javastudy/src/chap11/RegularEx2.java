f5f38016dde00da10b45b69bc7c15a9831a053da
DONGHUIS <134819640+DONGHUIS@users.noreply.github.com>
2023-11-11 17:23:29 +0900

    2023.11.11 일부코드 수정.

--- javastudy/src/chap11/RegularEx2.java
+++ javastudy/src/chap11/RegularEx2.java
@@ -29,23 +29,8 @@ public class RegularEx2 {
 				".","[a-z]?","[0-9]+","^[0-9]","[a-z]*",
 				"[a-z]+","02|010","\\s","\\S","\\d","\\w","\\W"};
 		String datas[] = {"","a","1","12","012","abc","02"," ","A","5","a","*"};
-		for(String d : datas) {
-			if(d.equals("")) {
-				System.out.print("빈문자\"\"의 패턴=>");
-			}else if(d.equals(" ")) {
-				System.out.print("공백문자\"\"의 패턴=>");
-			}else {
-				System.out.print(d+"문자의 패턴=>");
-			}
-			for(String p : patterns) {
-				Pattern pattern = Pattern.compile(p);
-				Matcher m = pattern.matcher(d);
-				if(m.matches()) {
-					System.out.print(p+",");
-				}
-			}
-			System.out.println();
-		}
+		
+		
 		
 	}
 }
